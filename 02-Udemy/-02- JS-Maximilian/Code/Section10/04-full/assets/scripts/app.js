a783701719827df8e38585e673d3ea8466b80a45
Ahmed Khalaf <ahmed1999khalafallah@gmail.com>
2023-02-09 15:30:14 +0200

    09-02-2023Tasks

--- 02-Udemy/-02- JS-Maximilian/Code/Section10/04-full/assets/scripts/app.js
+++ 02-Udemy/-02- JS-Maximilian/Code/Section10/04-full/assets/scripts/app.js
@@ -1,3 +1,5 @@
+// class one
+
 class Product {
   // title = 'DEFAULT';
   // imageUrl;
@@ -11,27 +13,37 @@ class Product {
     this.price = price;
   }
 }
+// class 2
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+      "A soft pillow!",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg",
+      "A carpet which you might like - or not.",
+      89.99
+    ),
+  ];
 
-class ShoppingCart {
-  items = [];
-
-  addProduct(product) {
-    this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
-  }
+  constructor() {}
 
   render() {
-    const cartEl = document.createElement('section');
-    cartEl.innerHTML = `
-      <h2>Total: \$${0}</h2>
-      <button>Order Now!</button>
-    `;
-    cartEl.className = 'cart';
-    this.totalOutput = cartEl.querySelector('h2');
-    return cartEl;
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    return prodList;
   }
 }
-
+// class 3
 class ProductItem {
   constructor(product) {
     this.product = product;
@@ -42,8 +54,8 @@ class ProductItem {
   }
 
   render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
     prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -55,46 +67,44 @@ class ProductItem {
           </div>
         </div>
       `;
-    const addCartButton = prodEl.querySelector('button');
-    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
     return prodEl;
   }
 }
 
-class ProductList {
-  products = [
-    new Product(
-      'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-      'A soft pillow!',
-      19.99
-    ),
-    new Product(
-      'A Carpet',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-      'A carpet which you might like - or not.',
-      89.99
-    )
-  ];
+class ShoppingCart {
+  items = [];
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+  }
+  get totalAmount() {
+    const sum = this.items.reduce((prev, curr) => curr.price + prev, 0);
+    return sum;
+  }
 
-  constructor() {}
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
 
   render() {
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
-    }
-    return prodList;
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
+    return cartEl;
   }
 }
 
 class Shop {
-  
   render() {
-    const renderHook = document.getElementById('app');
+    const renderHook = document.getElementById("app");
 
     this.cart = new ShoppingCart();
     const cartEl = this.cart.render();
@@ -121,7 +131,3 @@ class App {
 }
 
 App.init();
-
-
-
-
