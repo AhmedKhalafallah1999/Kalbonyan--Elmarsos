3c93d273c940b808215e3c16a64bb72813218642
Ahmed Khalaf <ahmed1999khalafallah@gmail.com>
2023-03-09 02:19:50 +0200

    update

---
+++ 02-Udemy/-02- JS-Maximilian/Code/Section17/async-05-first-promise/app.js
@@ -0,0 +1,48 @@
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
+function trackUserHandler() {
+  getPosition().then((posData) => {
+    console.log(posData);
+    setTimer(2000).then(data=>{
+      console.log(posData);
+    });
+  });
+  setTimer(1000).then(() => {
+    console.log("Timer done!");
+  });
+  console.log("Getting position...");
+}
+
+button.addEventListener("click", trackUserHandler);
+
+// let result = 0;
+
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
+
+// console.log(result);
